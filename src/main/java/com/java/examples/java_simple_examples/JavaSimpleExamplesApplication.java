539a90a8cdd5730ec0a2859cccd4d6b731b51e1e
mohammed SaleemUddin <mohammedsaleemuddin@mohammeds-MBP.lan>
2025-04-20 23:31:13 -0500

    initial commit

---
+++ src/main/java/com/java/examples/java_simple_examples/JavaSimpleExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.java.examples.java_simple_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSimpleExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/examples/java_simple_examples/JavaSimpleExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.examples.java_simple_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSimpleExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
